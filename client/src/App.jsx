5237cf1aa64ef30e7dabb0db3aa266c592a45021
Tarik Carey <renewed@gmail.com>
2025-02-28 22:39:10 -0700

    Initial Setup of React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
-
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
-      
-      <h1 className='text-red-500 text-5xl font-bold'>Hello World!</h1>
-      
+    <div className='absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+        <Route path='/auth' element={<AuthPage />} />
+				<Route path='/profile' element={<ProfilePage />} />
+				<Route path='/chat/:id' element={<MessagePage />} />
+			</Routes>
+    </div>
      
     </>
   )
